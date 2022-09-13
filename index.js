bf19858000c054a29d614511046bb5d6c7794149
TheLegendary1248 <1248alex@gmail.com>
2022-09-13 12:03:48 -0400

    Essentially finished

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 //Obtain user location
-function getLocation() {
+let coords;
+let destination;
+let map;
+const typeIds ={
+    cafe: 13032,
+    restaurant: 13065,
+    hotel: 19014,
+    market: 17114
+}
+const k = "fsq351qgi6/IEjxCCbINDNva3cCiqGVaaY+DNEzq1dF+YEw="
+const select = document.getElementById("select-type")
+let resultMarkers = []
+function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((e) => {
-            console.log(e)
-            let coords = e.coords
-            var map = L.map('map').setView([coords.latitude, coords.longitude], 13);
+            coords = e.coords
+            map = L.map('map').setView([coords.latitude, coords.longitude], 13);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '© OpenStreetMap'
             }).addTo(map);
-
-            L.marker([coords.latitude, coords.longitude], {title:"You are here"}).bindTooltip("You are here").addTo(map).openTooltip();
+            let self = L.marker([coords.latitude, coords.longitude], {title:"You are here"})
+            self.bindTooltip("You are here")
+            self.addTo(map)
+            self.openTooltip();
+            let range = L.circle([coords.latitude, coords.longitude], {radius: 16000, title: "range"});
+            range.addTo(map);
         });
-
-
     } else {
-        alert("oi")
+        alert("oi i need your location")
     }
 }
+async function getLocations(type){
+    let req = await fetch(`https://api.foursquare.com/v3/places/search?categories=${typeIds[type]}&radius=16000&ll=${coords.latitude},${coords.longitude}&limit=10`,{
+        headers: {
+            Accept: "application/json",
+            Authorization: k
+        }
+    })
+    let res = await req.json()
+    res.results.forEach(e => {
+        let self = L.marker([e.geocodes.main.latitude, e.geocodes.main.longitude], {title:e.name})
+        self.bindTooltip(e.name)
+        self.addTo(map)
+        self.openTooltip()
+    });
+}
 
-getLocation()
+//Opens a window with directions from user location to destination using Google maps
+function getDirections(to, from){
+    window.open(`https://www.google.com/maps/dir/${from.latitude},${from.longitude}/${to.latitude},${to.longitude}`)
+}
+function changedType(){
+    getLocations(select.value)
+}
 
+function showGoTo(name){
 
-//Load map
+}
+function HideGoTo(){
 
-//Get location selection filter
-//Display text information as well
+}
+initMap()
