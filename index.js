f87341a889304eeaf1106acceb5f66328084660c
TheLegendary1248 <1248alex@gmail.com>
2022-09-21 21:09:41 -0400

    I believe this is finished

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let coords;
 let destination;
 let map;
+//Type ID's for FSQR. I could not find how to get these programmatically through their API, if even possible
 const typeIds ={
     cafe: 13032,
     restaurant: 13065,
@@ -9,8 +10,31 @@ const typeIds ={
     market: 17114
 }
 const k = "fsq351qgi6/IEjxCCbINDNva3cCiqGVaaY+DNEzq1dF+YEw="
+//Type of business selection
 const select = document.getElementById("select-type")
+//Box that contains the information of the selected place and button for getting directions
+const infoBox = 
+{
+    container:document.getElementById("info"),
+    name: document.getElementById("name"),
+    addr: document.getElementById("addr"),
+    distance: document.getElementById("distance"),
+    button: document.getElementById("get-dir"),
+    //Takes the FSQR object and updates the info box accordingly
+    updateInfo(loc){
+         this.name.textContent = loc.name
+         this.addr.textContent = loc.location.address
+         this.distance.textContent = (loc.distance / 1000).toString().split(".")[0] + " km away"
+         this.button.onclick = 
+         () => getDirections([loc.geocodes.main.latitude, loc.geocodes.main.longitude], [coords.latitude, coords.longitude])
+         this.container.style.display = "block"
+    }
+}
+//Location results
+let locations = []
+//Markers of each location using Leaflet
 let resultMarkers = []
+//Initiallizes the map
 function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((e) => {
@@ -20,7 +44,7 @@ function initMap() {
                 maxZoom: 19,
                 attribution: '© OpenStreetMap'
             }).addTo(map);
-            let self = L.marker([coords.latitude, coords.longitude], {title:"You are here"})
+            let self = L.marker([coords.latitude, coords.longitude], {title:"You are here", draggable:true})
             self.bindTooltip("You are here")
             self.addTo(map)
             self.openTooltip();
@@ -31,34 +55,42 @@ function initMap() {
         alert("oi i need your location")
     }
 }
+//Function that gets locations return by FSQR
 async function getLocations(type){
+    //document.getElementById("map").classList.add("anim-map") Dont work yet
     let req = await fetch(`https://api.foursquare.com/v3/places/search?categories=${typeIds[type]}&radius=16000&ll=${coords.latitude},${coords.longitude}&limit=10`,{
         headers: {
             Accept: "application/json",
             Authorization: k
         }
     })
+    infoBox.container.style.display = "none"
+    //Clear markers
+    resultMarkers.forEach(e => map.removeLayer(e))
+    resultMarkers = []
+    //Add in new markers : ADD ERROR HANDLING
     let res = await req.json()
-    res.results.forEach(e => {
+    locations = res.results
+    locations.forEach(e => {
+        //Create marker
         let self = L.marker([e.geocodes.main.latitude, e.geocodes.main.longitude], {title:e.name})
-        self.bindTooltip(e.name)
+        //Add tooltip
+        self.bindTooltip(`<spanc class="tt-style">${e.name}</span>`, { permanent:true})
         self.addTo(map)
         self.openTooltip()
+        //Show location data on click event
+        self.location = e
+        self.on('click', () => {infoBox.updateInfo(self.location)})
+        resultMarkers.push(self)
     });
 }
 
 //Opens a window with directions from user location to destination using Google maps
 function getDirections(to, from){
-    window.open(`https://www.google.com/maps/dir/${from.latitude},${from.longitude}/${to.latitude},${to.longitude}`)
+    window.open(`https://www.google.com/maps/dir/${from[0]},${from[1]}/${to[0]},${to[1]}`)
 }
+//Called by the selection box onChange
 function changedType(){
     getLocations(select.value)
-}
-
-function showGoTo(name){
-
-}
-function HideGoTo(){
-
 }
 initMap()
